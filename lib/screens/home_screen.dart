b8436cf3141b885968dc5a1783a1a1004871b0cd
Victor Souza <victor.ti.souza@gmail.com>
2020-08-25 19:50:22 -0300

    update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('Ainda vou implementar :)'),
+      ),
+    );
   }
 }
